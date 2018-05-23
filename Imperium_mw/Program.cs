ad4d1572b34951cf13221752c7344aed0267c269
taltech <larsentag@gmail.com>
2018-05-23 13:01:59 +0200

    Add Excel read functionality

--- Imperium_mw/Program.cs
+++ Imperium_mw/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
-namespace Imperium_mw
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\ZoHo\data.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            Console.Write(xlWorksheet.Name.ToString() + " : " + rowCount.ToString());
+            Console.ReadKey();
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
         }
     }
 }
